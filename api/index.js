e01387c0124f3be6967f13c283d938bf6ce2bfdc
Sumit_Zende <sumitzende652@gmail.com>
2024-09-21 08:06:13 +0530

    mongoDB connected

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 import express from 'express'
+import  mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
+
+mongoose
+.connect(process.env.MOONGODB_URL)
+.then(()=>{
+    console.log('MongoDB is Connected succesfully');
+    
+});
 
 const app = express();
 
 app.listen(3000,()=>{
     console.log("Server running on port 3000!!");
-    
 })
