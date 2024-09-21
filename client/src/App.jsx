4114afb377716155e2f15a6d3446ff8142df0200
Sumit_Zende <sumitzende652@gmail.com>
2024-09-21 05:40:59 +0530

    created pages and routes them using react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {Home,SignIn,SignUp,About,Profile} from './pages/Pages'
 
 export default function App() {
   return (
-    <h1 className="text-red-600">Welcome</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/signin' element={<SignIn/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
