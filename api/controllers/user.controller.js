734ed72857ecfb5635eef48bcce396fdb5d1ff63
Sumit_Zende <sumitzende652@gmail.com>
2024-10-26 11:05:17 +0530

    created get user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs"
 import { errorHandler } from "../utils/error.js"
 import User from "../models/user.model.js"
+import Listing from "../models/listing.model.js"
 
 export const msg = (req,res)=>{
     res.json({
@@ -42,3 +43,17 @@ export const deleteUser=async(req,res,next)=>{
             next(error)
         }
 }
+
+
+export const getUserListing = async(req,res,next)=>{
+    if(req.params.id == req.params.id){
+        try {
+            const listings = await Listing.find({userRef : req.params.id})
+            res.status(200).json(listings);
+        } catch (error) {
+            next(error)
+        }
+    }else{
+      return next(errorHandler(401,'Not allowd to access'))
+    }
+  }
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUser, msg, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, getUserListing, msg, updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/updateUser.js';
 
 const router = express.Router();
@@ -7,5 +7,5 @@ const router = express.Router();
 router.get('/test',msg)
 router.post('/update/:id',verifyToken,updateUser)
 router.delete('/delete/:id',verifyToken,deleteUser)
-
+router.get('/listings/:id',verifyToken,getUserListing)
 export default router;
