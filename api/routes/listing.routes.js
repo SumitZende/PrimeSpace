57af4772ca11e502b2bd28b199ecd0a3039e548b
Sumit_Zende <sumitzende652@gmail.com>
2024-11-09 08:06:26 +0530

    created search api route

--- api/routes/listing.routes.js
+++ api/routes/listing.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../utils/updateUser.js';
-import { createListing, deleteListing, getListing, updateListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, getListing, searchListings, updateListing } from '../controllers/listing.controller.js';
 
 const router=express.Router();
 
@@ -8,4 +8,5 @@ router.post('/create',verifyToken,createListing);
 router.delete('/delete/:id',verifyToken,deleteListing)
 router.post('/update/:id',verifyToken,updateListing)
 router.get('/getListings/:id',getListing);
+router.get('/get',searchListings)
 export default router;
