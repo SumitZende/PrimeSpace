24eb914fc7e039613d4ccfe5cc2d89a93a54c550
Sumit_Zende <sumitzende652@gmail.com>
2024-11-16 03:56:15 +0530

    making footer component settle at the bottom

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <div className="bg-footer-color p-[10px] flex items-center justify-center">
+    <div className="bg-footer-color p-[10px] flex items-center justify-center fixed bottom-0 w-full">
       <Link
         to={`mailto:sumitzende1012@gmail.com?subject=Inquiry%20Regarding%20PrimeSpace%20Development`}
       >
-        <p className="text-white font-semibold "> ©sumitzende1012@gmail.com</p>
+        <p className="text-white font-semibold">©sumitzende1012@gmail.com</p>
       </Link>
     </div>
   );
 }
+
